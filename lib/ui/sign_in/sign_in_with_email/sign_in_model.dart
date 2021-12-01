8f801e9a73522104dc1caf260aeed51c9ed0ad81
mohamadElEaswy <MIDOO_303@YAHOO.COM>
2021-12-01 19:58:28 +0200

    1-12-2021

--- lib/ui/sign_in/sign_in_with_email/sign_in_model.dart
+++ lib/ui/sign_in/sign_in_with_email/sign_in_model.dart
@@ -1,8 +1,10 @@
+import 'package:timetracker/services/auth.dart';
 import 'package:timetracker/ui/sign_in/validators.dart';
+import 'package:flutter/foundation.dart';
 
 enum EmailSignInFormType { signIn, register }
 
-class EmailSignInModel with EmailAndPasswordValidation {
+class EmailSignInModel with EmailAndPasswordValidation, ChangeNotifier {
   EmailSignInModel({
     this.email = '',
     this.password = '',
@@ -57,5 +59,91 @@ class EmailSignInModel with EmailAndPasswordValidation {
       isLoading: isLoading ?? this.isLoading,
       submitted: submitted ?? this.submitted,
     );
+  }
+}
+
+class EmailSignInChangeModel with EmailAndPasswordValidation, ChangeNotifier {
+  EmailSignInChangeModel(
+      {this.email = '',
+      this.password = '',
+      this.isLoading = false,
+      this.submitted = false,
+      this.formType = EmailSignInFormType.signIn,
+      required this.auth});
+  final AuthBase auth;
+  String email;
+  String password;
+  bool isLoading;
+  bool submitted;
+  EmailSignInFormType formType;
+  String get primaryText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+  }
+
+  String get secondaryText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+  }
+
+  bool get submitEnabled {
+    return emailValidator.isValid(email) &&
+        emailValidator.isValid(password) &&
+        !isLoading;
+  }
+
+  String? get passwordErrorText {
+    bool passwordValid = submitted && passwordValidator.isValid(password);
+    return passwordValid ? invalidEmailErrorText : null;
+  }
+
+  String? get emailErrorText {
+    bool emailValid = submitted && emailValidator.isValid(email);
+    return emailValid ? invalidPasswordErrorText : null;
+  }
+
+  Future<void> submit() async {
+    updateWith(isLoading: true, submitted: true);
+    try {
+      if (formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(email, password);
+      } else {
+        await auth.createUserWithEmailAndPassword(email, password);
+      }
+    } catch (e) {
+      updateWith(isLoading: false, submitted: false);
+      rethrow;
+    }
+  }
+
+// toggle between signing in forms
+  void toggleFormType() {
+    final formType = this.formType == EmailSignInFormType.signIn
+        ? EmailSignInFormType.register
+        : EmailSignInFormType.signIn;
+    updateWith(
+      email: '',
+      password: '',
+      formType: formType,
+      submitted: false,
+      isLoading: false,
+    );
+  }
+
+  void updateWith({
+    String? email,
+    String? password,
+    EmailSignInFormType? formType,
+    bool? isLoading,
+    bool? submitted,
+  }) {
+    this.email = email ?? this.email;
+    this.password = password ?? this.password;
+    this.formType = formType ?? this.formType;
+    this.isLoading = isLoading ?? this.isLoading;
+    this.submitted = submitted ?? this.submitted;
+    notifyListeners();
   }
 }
