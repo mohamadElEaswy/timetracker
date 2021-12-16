8e30d81ca667d417867c4178aa34586cbee4187a
mohamadElEaswy <MIDOO_303@YAHOO.COM>
2021-12-17 00:54:12 +0200

    tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:timetracker/main.dart';
+import 'package:timetracker/ui/sign_in/validators.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('Non empty string', () {
+    final validators = NotEmptyStringValidator();
+    expect(validators.isValid('test'), true);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('empty string', () {
+    final validators = NotEmptyStringValidator();
+    expect(validators.isValid(''), false);
   });
 }
